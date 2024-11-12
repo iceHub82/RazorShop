f2d39aa7c91cec9736a475775f0b7e999ec90a32
Örvar Karlsson <orvar@secure-aid.com>
2024-11-12 15:27:47 +0100

    Add project files.

---
+++ RazorShop.Web/wwwroot/js/site.js
@@ -0,0 +1,10 @@
+﻿$(document).on('change', '#productSize', function() {
+    $('#checkedSize').val($(this).val());
+});
+
+//document.body.addEventListener("htmx:historyRestore", (e) => {
+//    console.log('historyRestore');
+//    const element = document.getElementById('your-element-id');
+//    // Perform any cleanup or adjustments to prevent duplicates
+//    element.innerHTML = "";  // Clear or reset as needed
+//});
